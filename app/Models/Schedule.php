72c4e67e2cf384beadc3fcc7f42ea5be8a89e9b8
Your Name <romain.poulain@edu.esiee_it.fr>
2025-02-17 23:13:59 +0100

    Migration Schedule

---
+++ app/Models/Schedule.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Schedule extends Model
+{
+        protected $table = 'schedules';
+        protected $fillable = [
+            'id',
+            'user_id',
+            'day_of_week',
+            'start_time',
+            'end_time',
+        ];
+
+    public function getFillable(): array
+    {
+        return $this->fillable;
+    }
+
+    public function setFillable(array $fillable): void
+    {
+        $this->fillable = $fillable;
+    }
+
+    public function getTable(): string
+    {
+        return $this->table;
+    }
+
+}
