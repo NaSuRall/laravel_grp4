c5931c6a7938c39ed14e938c9a161f0457efe035
Romain Poulain <romain.poulain@edu.esiee-it.fr>
2025-02-18 13:54:30 +0100

    ff

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -15,7 +15,7 @@
     @yield('custom_css')
 
     <!-- Scripts -->
-    <link rel="stylesheet" href="{{ mix('css/app.css') }}">
+    <link rel="styleshgit merge eet" href="{{ mix('css/app.css') }}">
     <script src="{{ mix('js/app.js') }}"></script>
 </head>
 <body>
