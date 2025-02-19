5fd00e3decea62e0f44f2b8f611a979166244c46
Romain Poulain <romain.poulain@edu.esiee-it.fr>
2025-02-19 10:00:44 +0100

    animation

--- resources/views/admin_schedule.blade.php
+++ resources/views/admin_schedule.blade.php
@@ -2,33 +2,38 @@
 
 @section('custom_css')
     <link rel="stylesheet" href="{{ asset('css/nav.css') }}">
-    <link rel="stylesheet" href="{{ asset('css/admin_appointment.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/admin_schedule.css') }}">
 @endsection
 
 @section('content')
     @include('elements.nav')
 
-    <form action="{{ route('send_form_admin') }}" method="post">
-        @csrf
-        <label for="day_of_week">Jour de la semaine :</label>
-        <select id="day_of_week" name="day_of_week" required>
-            <option value="Monday">Lundi</option>
-            <option value="Tuesday">Mardi</option>
-            <option value="Wednesday">Mercredi</option>
-            <option value="Thursday">Jeudi</option>
-            <option value="Friday">Vendredi</option>
-            <option value="Saturday">Samedi</option>
-            <option value="Sunday">Dimanche</option>
-        </select>
 
-        <label for="start_time">Heure de début :</label>
-        <input type="time" id="start_time" name="start_time" required>
+    <div class="formulaire">
+        <form action="{{ route('send_form_admin') }}" method="post">
+            @csrf
+            <label for="day_of_week">Jour de la semaine :</label>
+            <select id="day_of_week" name="day_of_week" required>
+                <option value="Monday">Lundi</option>
+                <option value="Tuesday">Mardi</option>
+                <option value="Wednesday">Mercredi</option>
+                <option value="Thursday">Jeudi</option>
+                <option value="Friday">Vendredi</option>
+                <option value="Saturday">Samedi</option>
+                <option value="Sunday">Dimanche</option>
+            </select>
 
-        <label for="end_time">Heure de fin :</label>
-        <input type="time" id="end_time" name="end_time" required>
+            <label for="start_time">Heure de début :</label>
+            <input type="time" id="start_time" name="start_time" required>
 
-        <input type="hidden" name="user_id" value="{{ auth()->id() }}">
+            <label for="end_time">Heure de fin :</label>
+            <input type="time" id="end_time" name="end_time" required>
 
-        <button type="submit">Envoyer</button>
-    </form>
+            <input type="hidden" name="user_id" value="{{ auth()->id() }}">
+            <div class="btn">
+                <button type="submit">Envoyer</button>
+            </div>
+        </form>
+    </div>
+
 @endsection
