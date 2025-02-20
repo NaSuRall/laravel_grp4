63dbddc65722e47e452cea98238dbc07561e68c7
lululu957 <lucasbaudingoblet@gmail.com>
2025-02-20 11:26:51 +0100

    Add contact us

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -59,26 +59,6 @@
             </div>
         </div>
 
-        <div class="centre">
-            <div class="contact">
-                <div>
-                    <form class="contact-form">
-                        <h3>Contactez-nous</h3>
-                        <label for="name">Nom :</label>
-                        <input type="text" id="name" name="name" required>
-
-                        <label for="email">Email :</label>
-                        <input type="email" id="email" name="email" required>
-
-                        <label for="message">Message :</label>
-                        <textarea id="message" name="message" rows="4" cols="55" required></textarea>
-
-                        <button type="submit">Envoyer</button>
-                    </form>
-                </div>
-            </div>
-        </div>
-
     <div class="plaging">
         <div class="titre">
             <h1>Créneaux disponibles</h1>
--- resources/views/myAccount.blade.php
+++ resources/views/myAccount.blade.php
@@ -7,11 +7,25 @@
 @section('content')
 
     @include('elements.nav')
-    <h1>Bonjour : {{ auth()->user()->name }} </h1>
-    @csrf
-    <form action="{{ route('send.email') }}" method="GET">
-        <button type="submit">Envoyer un E-mail</button>
-    </form>
+    <div class="centre">
+        <div class="contact">
+            <div>
+                <form class="contact-form">
+                    <h3>Contactez-nous</h3>
+                    <label for="name">Nom :</label>
+                    <input type="text" id="name" name="name" required>
+
+                    <label for="email">Objet :</label>
+                    <input type="text" id="objet" name="objet" required>
+
+                    <label for="message">Message :</label>
+                    <textarea id="message" name="message" rows="4" cols="55" required></textarea>
+
+                    <button type="submit">Envoyer</button>
+                </form>
+            </div>
+        </div>
+    </div>
 
 
 
