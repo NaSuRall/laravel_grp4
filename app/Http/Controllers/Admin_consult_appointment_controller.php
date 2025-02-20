3984bafcbd8a4dde34650b2c876f5531bdd60641
Romain Poulain <romain.poulain@edu.esiee-it.fr>
2025-02-20 17:03:49 +0100

    last

--- app/Http/Controllers/Admin_consult_appointment_controller.php
+++ app/Http/Controllers/Admin_consult_appointment_controller.php
@@ -5,7 +5,8 @@ namespace App\Http\Controllers;
 use App\Models\Appointment;
 use App\Models\Schedule;
 use Illuminate\Http\Request;
-
+use App\Models\User;
+use App\Http\Controllers\Auth;
 class Admin_consult_appointment_controller extends Controller
 {
     public function admin()
