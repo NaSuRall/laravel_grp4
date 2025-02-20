017541e3d6c03aecc6cd80fd64b1bc1433a5f971
Romain Poulain <romain.poulain@edu.esiee-it.fr>
2025-02-20 14:36:13 +0100

    commit for merge

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -9,24 +9,33 @@
     @include('elements.nav')
         <div class="row">
             <div class="column left">
-                <h1>Bonjour {{ auth()->user()->name }} ! Comment allez-vous ?</h1>
+
+                <div class="tt">
+                    <h1 class="azerty">Bonjour {{ auth()->user()->name }} ! Comment allez-vous ?</h1>
+                </div>
                 <div class="text">
-                    <p>
-                        Bienvenue sur notre site dédié à votre bien-être et à votre santé.</br>
-                        Que vous soyez en quête de rééducation, de soulagement des douleurs</br>
-                        musculaires ou d’amélioration de votre mobilité, nous sommes là </br>
-                        pour vous accompagner avec expertise et bienveillance.</br></br>
-                        Grâce à des techniques adaptées et un suivi personnalisé,</br>
-                        nous mettons tout en œuvre pour optimiser votre récupération.</br>
-                        Nous vous accueillons dans un cadre apaisant, propice</br>
-                        à votre remise en forme et à votre confort.</br>
-                        Chaque patient est unique, c'est pourquoi nous proposons</br>
-                        des soins individualisés pour répondre à vos besoins spécifiques.</br></br>
-                        Prenez rendez-vous dès aujourd’hui et offrez à votre corps</br>
-                        les soins qu’il mérite ! Nous restons à votre écoute</br>
-                        pour vous guider vers une meilleure qualité de vie.</br>
-                        Votre bien-être est notre priorité.</br>
-                    </p>
+                        <p>
+                        Bienvenue sur notre site dédié à votre bien-être et à votre santé.
+                        Que vous soyez en quête de rééducation, de soulagement des douleurs
+                        musculaires ou d’amélioration de votre mobilité, nous sommes là
+                        pour vous accompagner avec expertise et bienveillance. <br><br>
+                        </p>
+
+                        <p>
+                            Grâce à des techniques adaptées et un suivi personnalisé,
+                            nous mettons tout en œuvre pour optimiser votre récupération.
+                            Nous vous accueillons dans un cadre apaisant, propice
+                            à votre remise en forme et à votre confort.
+                            Chaque patient est unique, c'est pourquoi nous proposons
+                            des soins individualisés pour répondre à vos besoins spécifiques. <br> <br>
+                        </p>
+
+                        <p>
+                            Prenez rendez-vous dès aujourd’hui et offrez à votre corps
+                            les soins qu’il mérite ! Nous restons à votre écoute
+                            pour vous guider vers une meilleure qualité de vie.
+                            Votre bien-être est notre priorité.
+                        </p>
                 </div>
             </div>
 
@@ -44,16 +53,16 @@
             <div class="column right MedecinD">
                 <h2>Medecin</h2>
                 <p>
-                    Nos kinésithérapeutes sont sélectionnés selon des critères exigeants,</br>
-                    garantissant un haut niveau de compétence et d’expertise.</br>
-                    Tous diplômés d’écoles reconnues, ils bénéficient également</br>
-                    de formations continues pour rester à la pointe des avancées</br>
-                    en matière de rééducation et de soins musculaires.</br>
-                    Leur approche repose sur une écoute attentive, une évaluation</br>
-                    approfondie et des protocoles de soins adaptés à chaque patient.</br></br>
-                    En choisissant notre cabinet, vous faites confiance à une équipe</br>
-                    passionnée, dédiée à votre bien-être et soucieuse d’apporter</br>
-                    des résultats concrets et durables.</br>
+                    Nos kinésithérapeutes sont sélectionnés selon des critères exigeants,<br>
+                    garantissant un haut niveau de compétence et d’expertise.<br>
+                    Tous diplômés d’écoles reconnues, ils bénéficient également<br>
+                    de formations continues pour rester à la pointe des avancées<br>
+                    en matière de rééducation et de soins musculaires.<br>
+                    Leur approche repose sur une écoute attentive, une évaluation<br>
+                    approfondie et des protocoles de soins adaptés à chaque patient.<br><br>
+                    En choisissant notre cabinet, vous faites confiance à une équipe<br>
+                    passionnée, dédiée à votre bien-être et soucieuse d’apporter<br>
+                    des résultats concrets et durables.<br>
                 </p>
                 <button type="submit">Consulter</button>
             </div>
