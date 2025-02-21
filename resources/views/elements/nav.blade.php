66f74dca8fc9cfcf435d3877cd9a4ea759791b0f
Romain Poulain <romain.poulain@edu.esiee-it.fr>
2025-02-21 09:47:04 +0100

    check nav

--- resources/views/elements/nav.blade.php
+++ resources/views/elements/nav.blade.php
@@ -9,7 +9,8 @@
             <a href="{{ route('home')  }}">Accueil</a>
             <a href="{{ route('rendezvous') }}">Rendez-vous</a>
             <a href="{{ route('admin.showClient') }}"><span>Rendez-vous admin</span></a>
-            <a href="{{ route('admin.schedule') }}">Prendre rendez-vous</a>
+            <a href="{{ route('admin.schedule') }}">Creneaux Horraire</a>
             <a href="{{ route('myAcount') }}">{{ auth()->user()->name}}</a>
+            <a href="{{ route('logout') }}">Logout</a>
         </div>
     </div>
